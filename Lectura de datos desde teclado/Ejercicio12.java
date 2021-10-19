df3f7c01d3930b21bac061b5dd7b603bad116b46
Sergio Domínguez Gómez <91872827+SergioDominguez15@users.noreply.github.com>
2021-10-19 10:35:39 +0200

    Add files via upload

---
+++ Lectura de datos desde teclado/Ejercicio12.java
@@ -0,0 +1,11 @@
+public class Ejercicio12 {
+  public static void main(String[] args) {
+
+    System.out.println("------Programa para calcular nota que hace falta------");
+    System.out.print("Por favor, introduzca la nota del primer examen ");
+    double nota1 = Double.parseDouble(System.console().readLine());
+    System.out.print("¿Que nota quieres sacar en el trimestre? ");
+    double notatrimestre = Double.parseDouble(System.console().readLine());
+    System.out.println("Para sacar un " + notatrimestre + "en el trimestre necesitas sacar un" + ((notatrimestre - 0.4*nota1)/0.6) );
+  }
+}
