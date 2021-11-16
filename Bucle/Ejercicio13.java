93836176df895717b691a4ceb194cdc31f95a0e7
Sergio Domínguez Gómez <91872827+SergioDominguez15@users.noreply.github.com>
2021-11-16 08:42:10 +0100

    Add files via upload

---
+++ Bucle/Ejercicio13.java
@@ -0,0 +1,21 @@
+public class Ejercicio13 {
+
+  public static void main(String[] args) {
+  
+    System.out.println("Por favor, introduzca 10 números enteros: ");
+
+    int negativos = 0;
+    int positivos = 0;
+    
+    for (int i = 0; i < 10; i++) {
+      if (Integer.parseInt(System.console().readLine()) < 0) {
+        negativos++;
+      } else {
+        positivos++;
+      }
+    }
+    
+    System.out.println("Ha introducido " + positivos + " positivos y " + negativos + " negativos");
+
+  }
+}
---
+++ Bucle/Ejercicio14.java
@@ -0,0 +1,35 @@
+public class Ejercicio14 {
+
+  public static void main(String[] args) {
+    
+    System.out.println("Cálculo de una potencia");
+    
+    System.out.print("Introduzca la base: ");
+    int base = Integer.parseInt(System.console().readLine());
+    
+    System.out.print("Introduzca el exponente: ");
+    int exponente = Integer.parseInt(System.console().readLine());
+
+    double potencia = 1;
+        
+    if (exponente == 0) {
+      potencia = 1;
+    }
+    
+    if (exponente > 0) {
+      for (int i = 0; i < exponente; i++) {
+        potencia *= base;
+      }
+    }
+    
+    if (exponente < 0) {
+      for (int i = 0; i < -exponente; i++) {
+        potencia *= base;
+      }
+      
+      potencia = 1/potencia;
+    }
+
+    System.out.println(base + "^" + exponente + " = " + potencia);
+  }
+}
